3d000a38aab7eaacc102d570cc75d90a46532152
HappySummer <zjhao233@126.com>
2026-04-20 15:23:59 +0800

    feat(响应式布局): 添加屏幕断点并更新导航响应式逻辑
    - 在tailwind配置中添加sm-md-lg-xl-2xl屏幕断点
    - 更新Header组件导航栏的响应式断点从lg改为xl
    - 升级vite及相关插件版本

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1300px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         primary: {
