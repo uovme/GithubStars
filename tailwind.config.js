d9757bcc40052742d5ccac3fd2e149835483aab0
AmintaCCCP <libogege@sina.com>
2026-04-22 14:23:44 +0800

    Revert "feat(响应式布局): 添加屏幕断点并更新导航响应式逻辑"
    This reverts commit 5f6212b53bb7f743e3c7ab7e1cac2f8dc3410268.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1300px',
-      '2xl': '1536px',
-    },
     extend: {
       colors: {
         primary: {
