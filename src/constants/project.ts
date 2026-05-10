9fb9963dd8a22e4a9f85345a775dd32ecb3aca8f
noaul <noaul@users.noreply.github.com>
2026-05-10 17:35:27 +0800

    fix: point PROJECT_REPO_URL to fork repo uovme/GithubStars

--- src/constants/project.ts
+++ src/constants/project.ts
@@ -1,3 +1,3 @@
-export const PROJECT_REPO_URL = 'https://github.com/AmintaCCCP/GithubStarsManager';
+export const PROJECT_REPO_URL = 'https://github.com/uovme/GithubStars';
 
 export const PROJECT_ISSUES_URL = `${PROJECT_REPO_URL}/issues`;
